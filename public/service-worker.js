95fd884e785128b6f70edcb457bf57f6db78b124
Bryson Vatuvei <vatuvei.bryson@yahoo.com>
2022-08-19 12:33:35 -0600

    added service worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,69 +1,72 @@
-const APP_PREFIX = 'FoodEvent-';     
-const VERSION = 'version_01';
-const CACHE_NAME = APP_PREFIX + VERSION;
 const FILES_TO_CACHE = [
   "./index.html",
-  "./events.html",
-  "./tickets.html",
-  "./schedule.html",
-  "./assets/css/style.css",
-  "./assets/css/bootstrap.css",
-  "./assets/css/tickets.css",
-  "./dist/app.bundle.js",
-  "./dist/events.bundle.js",
-  "./dist/tickets.bundle.js",
-  "./dist/schedule.bundle.js"
+  "./manifest.json",
+  "./css/styles.css",
+  "./icons/icon-72x72.png",
+  "./icons/icon-96x96.png",
+  "https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css",
+  "https://cdn.jsdelivr.net/npm/chart.js@2.8.0",
+  "./icons/icon-128x128.png",
+  "./icons/icon-144x144.png",
+  "./icons/icon-152x152.png",
+  "./icons/icon-192x192.png",
+  "./icons/icon-384x384.png",
+  "./icons/icon-512x512.png",
+  "./js/idb.js",
+  "./js/index.js",
 ];
+const APP_PREFIX = "Budget-";
+const VERSION = "version_01";
+const CACHE_NAME = APP_PREFIX + VERSION;
 
 // Respond with cached resources
-self.addEventListener('fetch', function (e) {
-  console.log('fetch request : ' + e.request.url)
-  e.respondWith(
-    caches.match(e.request).then(function (request) {
-      if (request) { // if cache is available, respond with cache
-        console.log('responding with cache : ' + e.request.url)
-        return request
-      } else {       // if there are no cache, try fetching request
-        console.log('file is not cached, fetching : ' + e.request.url)
-        return fetch(e.request)
-      }
-
+self.addEventListener("install", function (e) {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
       // You can omit if/else for console.log & put one line below like this too.
       // return request || fetch(e.request)
     })
-  )
-})
+  );
+});
 
 // Cache resources
-self.addEventListener('install', function (e) {
+self.addEventListener("activate", function (e) {
   e.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log('installing cache : ' + CACHE_NAME)
-      return cache.addAll(FILES_TO_CACHE)
-    })
-  )
-})
-
-// Delete outdated caches
-self.addEventListener('activate', function(e) {
-  e.waitUntil(
-    caches.keys().then(function(keyList) {
-      // `keyList` contains all cache names under your username.github.io
-      // filter out ones that has this app prefix to create keeplist
-      let cacheKeeplist = keyList.filter(function(key) {
+    caches.keys().then(function (keyList) {
+      let cacheKeeplist = keyList.filter(function (key) {
         return key.indexOf(APP_PREFIX);
       });
-      // add current cache name to keeplist
       cacheKeeplist.push(CACHE_NAME);
-
       return Promise.all(
-        keyList.map(function(key, i) {
+        keyList.map(function (key, i) {
           if (cacheKeeplist.indexOf(key) === -1) {
-            console.log('deleting cache : ' + keyList[i]);
+            console.log("deleting cache : " + keyList[i]);
             return caches.delete(keyList[i]);
           }
         })
       );
     })
+  );
+});
+
+// Delete outdated caches
+self.addEventListener("fetch", function (e) {
+  console.log("fetch request:" + e.request.url);
+  e.respondWith(
+    caches
+      .match(e.request)
+      .then(function (response) {
+        if (response) {
+          console.log("response of fetch event listener", response);
+          return response;
+        } else {
+          return fetch(e.request);
+        }
+      })
+      // add current cache name to keeplist
+      .catch((error) => {
+        console.log("response of error", error);
+      })
   );
 });
